5ca7cbb75fb52e6a2390f1ea7d458b91641aebe9
Karen V <karenvo@usc.edu>
2019-09-21 04:56:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     if req.path =="/items"
       resp.write "#{item.price}"
     else
-      resp.write "Item not found"
+      resp.write "Route not found"
       resp.status = 404
     end
     resp.finish
