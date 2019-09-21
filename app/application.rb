e977744e55c3d3b69c81a59f34a80eddaf79a36d
Karen V <karenvo@usc.edu>
2019-09-21 05:17:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,16 +3,26 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path =="/items"
-      resp.write "#{item.price}"
-      resp.status = 200
-    elsif req.path =="/items"
-      resp.write "Item not found"
-      resp.status = 404
+    if req.path.match(/items/)
+        Item.each do |item|
+          resp.write "#{Item.price}"
+          resp.status = 200
+        end
+    # elsif req.path =="/items"
+    #   resp.write "Item not found"
+    #   resp.status = 404
     else
       resp.write "Route not found"
       resp.status = 404
     end
     resp.finish
+  end
+end
+
+def handle_search(search_term)
+  if Item.include?(search_term)
+    adfad
+  else
+    ealf
   end
 end
