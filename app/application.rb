815ce5b2dddd0d19bc25ae5182b8a1ef980eab68
Karen V <karenvo@usc.edu>
2019-09-21 05:57:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
             item_match = req.path.split("/items/").last
             item = @@items.find{|i| i.name == item_match}
           if item_match == item
-            resp.write "#{@@item.price}"
+            resp.write "#{item[1]}"
             resp.status = 200
           else
             resp.write "Item not found"
