a0901a25d5d316dd30c7f22ca709b0f910e99b10
Karen V <karenvo@usc.edu>
2019-09-21 05:53:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,7 @@ class Application
       #     resp.write "#{item[1]}"
         if item_match = req.path.split("/items/").last
           item = @@items.find{|i| i.name == item_match}
-          resp.write "#{item[1]}"
+          resp.write "#{item}"
           resp.status = 200
           else
             resp.write "Item not found"
