46d0abdf524cb0fa4f4bcfc55e57bd0dd520d940
Karen V <karenvo@usc.edu>
2019-09-21 05:21:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,10 +12,10 @@ class Application
           resp.write "Item not found"
           resp.status = 404
         end
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
     resp.finish
   end
 end
